baadb0e04ee8da59085a33f38928e7ded3006c62
Hussam Hanafi <twindoc2000@gmail.com>
2019-10-24 20:13:19 +0200

    Final touches on the model and uploading the report

--- Model.R
+++ Model.R
@@ -67,15 +67,26 @@ RMSE <- function(x,y){
 
 #Predicting using only the mean
 mu <- mean(train$rating)
-naive_rmse <- RMSE(validation$rating, mu)
+naive_rmse <- RMSE(train$rating, mu)
 
 #Results data frame
-rmse_results <- data_frame(method = "Just the average", RMSE = naive_rmse, Lamda = NA)
+rmse_results <- data_frame(Method = "Just the average", RMSE = naive_rmse, Lamda = NA)
 
 ## Modelling the Movie Effect
-#Finding optimal Lamda Parameter
-l <- seq(0,10,0.25)
+l <- 5
 
+b_i <- train %>% group_by(movieId) %>% 
+  summarize(b_i = sum(rating - mu)/(n()+l), n = n())
+
+b_i %>% ggplot(aes(b_i)) + geom_histogram(bins = 15, color = "black")
+
+b_i %>% sample_n(30) %>%
+  ggplot(aes(as.character(movieId), b_i, size=n)) + 
+  geom_point() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + xlab("movieId")
+
+#Finding optimal Lamda Parameter
+l <- seq(0,20,0.25)
 RMSE_lamda <- map_df(l,function(l){
 
   b_i <- train %>% group_by(movieId) %>% 
@@ -93,15 +104,31 @@ RMSE_lamda %>% ggplot(aes(l,RMSE)) + geom_point()
 #Inputing results
 result <- min(RMSE_lamda$RMSE)
 l <- RMSE_lamda$l[which.min(RMSE_lamda$RMSE)]
-rmse_results <- bind_rows(rmse_results, data_frame(method = "Mean + b_i", RMSE = result, Lamda = l))
+rmse_results <- bind_rows(rmse_results, data_frame(Method = "Mean + b_i", RMSE = result, Lamda = l))
 
 #Saving the optimal Movie Effect
 b_i <- train %>% group_by(movieId) %>% 
   summarize(b_i = sum(rating - mu)/(n() + l))
 
 ## Modelling the User Effect
+
+# Analysing the user Effect with an arbitrary lambda
+l<- 5
+
+b_u <- train %>% left_join(b_i, by = "movieId") %>% group_by(userId) %>% 
+  summarize(b_u = sum(rating - mu - b_i)/(n()+l), n = n())
+
+b_u %>% sample_n(30) %>% 
+  ggplot(aes(as.character(userId), b_u, size = n, alpha = 0.3)) + 
+  geom_point() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust =1)) + 
+  xlab("userId")
+
+b_u %>% ggplot(aes(b_u)) + geom_histogram(bins = 15, color = "black")
+
+
 #Finding optimal Lamda Parameter
-l <- seq(10,20,0.25)
+l <- seq(0,20,0.25)
 RMSE_lamda <- map_df(l,function(l){
 
   b_u <- train %>% left_join(b_i, by = "movieId") %>% group_by(userId) %>% 
@@ -119,13 +146,29 @@ RMSE_lamda %>% ggplot(aes(l,RMSE)) + geom_point()
 #Inputing reults
 result <- min(RMSE_lamda$RMSE)
 l <- RMSE_lamda$l[which.min(RMSE_lamda$RMSE)]
-rmse_results <- bind_rows(rmse_results, data_frame(method = "Mean + b_i + b_u", RMSE = result, Lamda = l))
+rmse_results <- bind_rows(rmse_results, data_frame(Method = "Mean + b_i + b_u", RMSE = result, Lamda = l))
 
 #Saving Optimal User effect
 b_u <- train %>% left_join(b_i, by = "movieId") %>% group_by(userId) %>% 
   summarize(b_u = sum(rating - mu - b_i)/(n() + l))
 
 ## Modelling the Genre Effect
+
+#Exploration of the genre effect with an arbitrary Lamda
+l <- 5
+
+b_g <- train %>% left_join(b_i, by= "movieId") %>% left_join(b_u, by = "userId") %>% 
+  group_by(genres) %>%
+  summarize(b_g = sum(rating - mu - b_i - b_u)/(n()+l), n = n()) 
+
+#Visualising the Genre effect
+
+b_g %>% sample_n(20) %>% ggplot(aes(genres, b_g, size = n, alpha = 0.3)) + 
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 30, hjust = 1))
+
+b_g %>% ggplot(aes(b_g)) + geom_histogram(bins = 15, color = "black")
+
 #Finding optimal Lamda Parameter
 l <- seq(0,10,0.25)
 RMSE_lamda <- map_df(l,function(l){
@@ -146,7 +189,7 @@ RMSE_lamda %>% ggplot(aes(l,RMSE)) + geom_point()
 #Inputing reults
 result <- min(RMSE_lamda$RMSE)
 l <- RMSE_lamda$l[which.min(RMSE_lamda$RMSE)]
-rmse_results <- bind_rows(rmse_results, data_frame(method = "Mean + b_i + b_u + b_g", RMSE = result, Lamda = l))
+rmse_results <- bind_rows(rmse_results, data_frame(Method = "Mean + b_i + b_u + b_g", RMSE = result, Lamda = l))
 
 #Saving Optimal Genre effect
 b_g <- train %>% left_join(b_i, by= "movieId") %>% left_join(b_u, by = "userId") %>% group_by(genres) %>%
@@ -162,19 +205,16 @@ l <- 5
 
 b_g <- train %>% left_join(b_i, by= "movieId") %>% left_join(b_u, by = "userId") %>% 
   group_by(genres) %>%
-  summarize(b_g = sum(rating - mu - b_i - b_u)/(n()+l), 
-            sd = sqrt(sum((rating- mu - b_i - b_u) - b_g)^2/(n()-1))) 
+  summarize(b_g = sum(rating - mu - b_i - b_u)/(n()+l), n = n())
 
 #Visualising the new Genre effect
-b_g %>% ggplot(aes(genres, b_g, ymax = b_g + sd, ymin = b_g - sd)) + geom_point() + geom_errorbar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+b_g %>% ggplot(aes(genres, b_g, size = n)) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-#Visualising the new Genre effect after filtering movies without listed genres
-b_g %>% filter(genres != "(no genres listed)") %>%
-  ggplot(aes(genres, b_g, ymax = b_g + sd, ymin = b_g - sd)) + geom_point() + geom_errorbar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
+#Visualising the distribution
+b_g %>% ggplot(aes(b_g)) + geom_histogram(color = "black")
 
 #Model
-l <- seq(0,10,0.25)
+l <- seq(0,20,0.25)
 RMSE_lamda <- map_df(l,function(l){
 
   b_g <- train %>% left_join(b_i, by= "movieId") %>% left_join(b_u, by = "userId") %>% group_by(genres) %>%
@@ -192,7 +232,7 @@ RMSE_lamda %>% ggplot(aes(l,RMSE)) + geom_point()
 #Inputing reults
 result <- min(RMSE_lamda$RMSE)
 l <- RMSE_lamda$l[which.min(RMSE_lamda$RMSE)]
-rmse_results <- bind_rows(rmse_results, data_frame(method = "mu + b_i + b_u + detailed_b_g", RMSE = result, Lamda = l))
+rmse_results <- bind_rows(rmse_results, data_frame(Method = "mu + b_i + b_u + detailed_b_g", RMSE = result, Lamda = l))
 
 #Saving Optimal Genre effect
 b_g <- train %>% left_join(b_i, by= "movieId") %>% left_join(b_u, by = "userId") %>% group_by(genres) %>%
@@ -214,16 +254,18 @@ l <- 5
 
 b_y <- train %>% left_join(b_i, by = "movieId") %>% left_join(b_u, by = "userId") %>% left_join(b_g, by = "genres") %>%
   group_by(year) %>%
-  summarize(b_y = sum(rating - mu - b_i - b_u - b_g)/(n()+l), sd = sqrt(sum((rating- mu - b_i - b_u - b_g) - b_y)^2/(n()-1)))
+  summarize(b_y = sum(rating - mu - b_i - b_u - b_g)/(n()+l), n = n())
 
-b_y %>% ggplot(aes(year, b_y, ymax = b_y + sd, ymin = b_y - sd)) + 
-  geom_point() + geom_errorbar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+b_y %>% ggplot(aes(as.numeric(year), b_y, size = n, alpha = 0.3)) + 
+  geom_point() + xlab("year")
 
-b_y %>% mutate(year = as.numeric(year)) %>% ggplot(aes(x = year,y = b_y)) + 
-  geom_smooth() + geom_point()
+b_y %>% ggplot(aes(as.numeric(year),y = b_y)) + 
+  geom_smooth() + geom_point() + xlab("year")
+
+b_y %>% ggplot(aes(b_y)) + geom_histogram(color = "black")
 
 #Model
-l <- seq(0,10,0.25)
+l <- seq(0,20,0.25)
 RMSE_lamda <- map_df(l,function(l){
   
   b_y <- train %>% left_join(b_i, by = "movieId") %>% left_join(b_u, by = "userId") %>% left_join(b_g, by = "genres") %>%
@@ -243,7 +285,7 @@ RMSE_lamda %>% ggplot(aes(l,RMSE)) + geom_point()
 #Inputing reults
 result <- min(RMSE_lamda$RMSE)
 l <- RMSE_lamda$l[which.min(RMSE_lamda$RMSE)]
-rmse_results <- bind_rows(rmse_results, data_frame(method = "mu + b_i + b_u + detailed_b_g + b_y", RMSE = result, Lamda = l))
+rmse_results <- bind_rows(rmse_results, data_frame(Method = "mu + b_i + b_u + detailed_b_g + b_y", RMSE = result, Lamda = l))
 
 #Saving Optimal release year effect
 b_y <- train %>% left_join(b_i, by = "movieId") %>% left_join(b_u, by = "userId") %>% left_join(b_g, by = "genres") %>%
@@ -252,20 +294,30 @@ b_y <- train %>% left_join(b_i, by = "movieId") %>% left_join(b_u, by = "userId"
 
 #Final Model
 
-
-
-RMSE_Final <- function(validation){
+Model <- function(validation){
   validation <- validation %>% separate_rows(genres, sep = "\\|")
   validation <- validation %>% 
-    mutate(year = str_extract(title,"\\(\\d{4}\\)")) %>% mutate(year = str_replace_all(year, "[//(//)]", ""))
+    mutate(year = str_extract(title,"\\(\\d{4}\\)")) %>% 
+    mutate(year = str_replace_all(year, "[//(//)]", ""))
   
-  pred <- validation %>% left_join(b_i, by = "movieId") %>% 
+  validation %>% left_join(b_i, by = "movieId") %>% 
     left_join(b_u, by = "userId") %>% 
     left_join(b_g, by = "genres") %>% 
     left_join(b_y, by = "year") %>%
     mutate(pred = mu + b_i + b_u + b_g + b_y) %>% pull(pred)
-  
-  RMSE(pred,validation$rating)
 }  
 
-RMSE_Final(validation)
+
+RMSE_final <- function(validation,pred){
+  validation <- validation %>% separate_rows(genres, sep = "\\|")
+  validation <- validation %>% 
+    mutate(year = str_extract(title,"\\(\\d{4}\\)")) %>% 
+    mutate(year = str_replace_all(year, "[//(//)]", ""))
+  
+  RMSE(validation$rating,pred)
+  
+}
+
+pred <- Model(validation)
+
+RMSE_final(validation,pred)
